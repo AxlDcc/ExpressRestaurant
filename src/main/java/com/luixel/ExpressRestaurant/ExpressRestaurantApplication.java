c2db52ec628c82b5371d9efde3211b87d7d6b3b1
DESKTOP-B79ULPG\Axeldcc <axeldcc@gmail.com>
2021-01-26 11:30:53 -0600

    Init commit

---
+++ src/main/java/com/luixel/ExpressRestaurant/ExpressRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.luixel.ExpressRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luixel/ExpressRestaurant/ExpressRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luixel.ExpressRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
